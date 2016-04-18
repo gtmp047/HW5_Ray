48b68e2d6a24a0bb12d05fe7ae6319d6abd28866
gtmp047 <Apr12901>
2016-04-18 22:21:59 +0300

    Edit Gui

--- HW5_Ray/Form1.Designer.cs
+++ HW5_Ray/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,7 +47,7 @@
             this.aboutToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(784, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(733, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -61,7 +62,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(12, 95);
+            this.label1.Location = new System.Drawing.Point(12, 96);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(213, 24);
             this.label1.TabIndex = 1;
@@ -69,35 +70,37 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(261, 97);
+            this.textBox1.Location = new System.Drawing.Point(261, 98);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(258, 22);
+            this.textBox1.Size = new System.Drawing.Size(370, 22);
             this.textBox1.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(525, 97);
+            this.button1.Location = new System.Drawing.Point(637, 98);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(84, 23);
             this.button1.TabIndex = 3;
             this.button1.Text = "Найти";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(525, 63);
+            this.button2.Location = new System.Drawing.Point(637, 63);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(84, 23);
             this.button2.TabIndex = 24;
             this.button2.Text = "Обзор";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(261, 63);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(258, 22);
+            this.textBox2.Size = new System.Drawing.Size(370, 22);
             this.textBox2.TabIndex = 23;
             // 
             // label2
@@ -110,11 +113,19 @@
             this.label2.TabIndex = 22;
             this.label2.Text = "Введите имя файла";
             // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(16, 127);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(705, 399);
+            this.treeView1.TabIndex = 25;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 171);
+            this.ClientSize = new System.Drawing.Size(733, 538);
+            this.Controls.Add(this.treeView1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
@@ -143,6 +154,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TreeView treeView1;
     }
 }
 
--- HW5_Ray/Form1.cs
+++ HW5_Ray/Form1.cs
@@ -6,12 +6,14 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace HW5_Ray
 {
     public partial class Form1 : Form
     {
+        string filePath = "";
         public Form1()
         {
             InitializeComponent();
@@ -23,6 +25,26 @@ namespace HW5_Ray
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "D:\\gtmp\\Documents\\VisualStudio\\Projects\\Data_Romanenkov\\HW3_EX_Hafman";
+            openFileDialog1.Filter = "All files (*.*)|*.*";
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+                textBox1.Text = filePath;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
