fbd6d7f2bcbde7c71b1caa2732990bfeff363d23
gtmp047 <Apr12901>
2016-04-19 20:26:20 +0300

    Inserting

--- HW5_Ray/Form1.cs
+++ HW5_Ray/Form1.cs
@@ -11,9 +11,55 @@ using System.Windows.Forms;
 
 namespace HW5_Ray
 {
+    
+
     public partial class Form1 : Form
     {
         string filePath = "";
+       public class Node
+        {
+            public Char Value;
+            public Node(char c)
+            {
+                Value = c;
+            }
+        }
+        TreeNode Find (TreeNodeCollection arr, string str) // хз почему но стандартный метод не робит
+        {
+            foreach (TreeNode item in arr)
+            {
+                if (item.Text.CompareTo(str) == 0)
+                    return item;
+            }
+            return null;
+        }
+        void InsertElement(String str)
+        {
+            Insert(null ,str);
+        }
+        void Insert(TreeNode tn, String str)
+        { 
+            TreeNode newTN = null;
+            if (str.Length == 0)
+                return;
+            char elem = str[0];
+            if(tn != null)
+            {
+                if (Find(tn.Nodes, elem.ToString()) == null)
+                    newTN = tn.Nodes.Add(elem.ToString());
+                else
+                    newTN = Find(tn.Nodes, elem.ToString());
+            }
+            else
+            {
+                
+                if (Find(treeView1.Nodes, elem.ToString())==null)
+                    newTN = treeView1.Nodes.Add(elem.ToString());
+                else
+                    newTN = Find(treeView1.Nodes, elem.ToString());
+            }
+            Insert(newTN, str.Remove(0, 1));
+        }
         public Form1()
         {
             InitializeComponent();
@@ -25,25 +71,28 @@ namespace HW5_Ray
         }
 
         private void Form1_Load(object sender, EventArgs e)
-        {
-
+        {   
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             Stream myStream = null;
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
-
-            openFileDialog1.InitialDirectory = "D:\\gtmp\\Documents\\VisualStudio\\Projects\\Data_Romanenkov\\HW3_EX_Hafman";
+            string[] dataFile;
+            openFileDialog1.InitialDirectory = "D:\\gtmp\\Documents\\VisualStudio\\Projects\\Data_Romanenkov\\HW5_Ray";
             openFileDialog1.Filter = "All files (*.*)|*.*";
             openFileDialog1.RestoreDirectory = true;
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 filePath = openFileDialog1.FileName;
                 textBox1.Text = filePath;
+                dataFile = File.ReadAllText(filePath).Split(new char[] { ' ' });
+                foreach (var item in dataFile)
+                {
+                    InsertElement(item);
+                }
             }
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
 
